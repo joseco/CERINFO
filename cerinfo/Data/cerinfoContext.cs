8f233d701f5c36c8280a4c76c16834a9dc90c346
Angel Chavez <anderzonmc@gmail.com>
2019-04-12 21:42:21 -0400

    Adicionando primeros archivos

---
+++ cerinfo/Data/cerinfoContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace cerinfo.Models
+{
+    public class cerinfoContext : DbContext
+    {
+        public cerinfoContext (DbContextOptions<cerinfoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<cerinfo.Models.usuario> usuario { get; set; }
+    }
+}
---
+++ cerinfo/Data/dbinicializador.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using cerinfo.Models;
+
+namespace cerinfo.Data
+{
+    public class dbinicializador
+    {
+        public static void Inicializador (cerinfoContext context)
+        {
+            context.Database.EnsureCreated();
+
+            //buscar si existen registros en la tabla categoria
+
+            if (context.usuario.Any())
+            {
+                return;
+            }
+           // var usuarios = new usuario[]
+            {
+               // new usuario{nombre_usuario="Angel",paterno_usuario="Chavez",materno_usuario="valverde"
+                //,tipo_usuario="Administrador",correo="anderzonmc@gmail.com",contrasena="123123"}
+            }
+        }
+    }
+}
